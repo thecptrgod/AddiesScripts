53abd06c39d9bcab40a071bb8bd11e15e83ea357
Addison Chernow <hello@addisonchernow.com>
2022-06-07 13:53:06 -0500

    Resize Image Script 0.1

---
+++ resizeimages.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+#0.1 -- 6/7/2022 -- Inital version. Has ZERO flexibility in it. Working on adding that soon!
+
+
+find . -name '*.png' -execdir mogrify -resize 80x60 -verbose {} +
+find . -name '*.PNG' -execdir mogrify -resize 80x60 -verbose {} +
+find . -name '*.jpg' -execdir mogrify -resize 80x60 -verbose {} +
+find . -name '*.JPG' -execdir mogrify -resize 80x60 -verbose {} +
+find . -name '*.gif' -execdir mogrify -resize 80x60 -verbose {} +
+find . -name '*.GIF' -execdir mogrify -resize 80x60 -verbose {} +
