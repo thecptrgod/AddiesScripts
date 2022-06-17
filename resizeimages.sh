9186e4960b81c7e676685a37b2b949b370b97188
thecptrgod <hello@addisonchernow.com>
2022-06-17 18:02:03 -0500

    addie testing

--- resizeimages.sh
+++ resizeimages.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 #0.1 -- 6/7/2022 -- Inital version. Has ZERO flexibility in it. Working on adding that soon!
-
+#0.11 - Testing my git connection
 
 find . -name '*.png' -execdir mogrify -resize 80x60 -verbose {} +
 find . -name '*.PNG' -execdir mogrify -resize 80x60 -verbose {} +
